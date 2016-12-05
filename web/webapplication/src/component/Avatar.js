68b3d716a4992db51c464c4a492df56c053a4447
joowon <saven7788@gmail.com>
2016-12-06 01:12:45 +0900

    modal issue complete

--- web/webapplication/src/component/Avatar.js
+++ web/webapplication/src/component/Avatar.js
@@ -4,8 +4,11 @@
 import React, {Component} from 'react';
 import '../css/App.css'
 import imgsrc from '../images/test_avatar.jpg'
+import imgsrc1 from '../images/22.png'
 import {Modal, Button, Checkbox} from 'react-bootstrap'
 import * as firebase from 'firebase';
+import * as actions from '../actions/index';
+
 
 
 class Avatar extends Component {
@@ -13,15 +16,17 @@ class Avatar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showModal: false
+            showModal: false,
+            statement: "오뎅날씨",
+            statement1: "코트날씨"
         };
         this.open = this.open.bind(this);
         this.close = this.close.bind(this);
 
-        super();
-        this.state = {
-            statement: "오뎅날씨"
-        };
+        // super();
+        // this.state = {
+        //
+        // };
     }
 
     close() {
@@ -36,7 +41,7 @@ class Avatar extends Component {
         });
     }
 
-    componentDidMount() {
+    getInitialState() {
         const rootRef = firebase.database().ref().child('weather');
         const statementRef = rootRef.child('statement');
 
@@ -61,12 +66,12 @@ class Avatar extends Component {
                     <Modal.Body>
                         <Checkbox inline>
                             1.<p> {this.state.statement}</p>
-                            <img src={imgsrc} className="img-responsive center-block" alt="아바타" width="500"/>
+                            <img src={imgsrc} className="img-responsive center-block" width="500"/>
                         </Checkbox>
                         {' '}
                         <Checkbox inline>
-                            2.<p> 코트날씨</p>
-                            <img src={imgsrc} className="img-responsive center-block" alt="아바타" width="500"/>
+                            2.<p> {this.state.statement1}</p>
+                          <img src={imgsrc1} className="img-responsive center-block" alt="아바타" width="500"/>
                         </Checkbox>
                         {' '}
                     </Modal.Body>
